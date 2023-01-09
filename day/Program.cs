7d5a5d4b69042b6873dd5b1bfcb6905508c48ba2
rinkuberde <rinkuberde@gmail.com>
2023-01-09 11:35:00 +0530

    [Rinku]- flip

---
+++ day/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            FlipCoin flipCoin = new FlipCoin();
+            flipCoin.PercentageFlips(10);
+        }
+    }
+}
